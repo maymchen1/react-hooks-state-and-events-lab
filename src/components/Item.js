b60adcb2b91e43d133b986b53f4e68c9f6e0286c
May <maymchen1@gmail.com>
2022-09-22 17:59:43 -0400

    Done.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [added, setAdded] =useState(false);
+  const displayButtonText = () => added ? "Remove from Cart" : "Add to Cart";
   return (
-    <li className="">
+    <li className={added ? 'in-cart': ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={()=> setAdded(!added)}>{displayButtonText()}</button>
     </li>
   );
 }
-
+//go to components to see state//
+//can see hooks - state//
+//whenever logic use curly brackets//
 export default Item;
